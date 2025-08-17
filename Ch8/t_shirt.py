14042668ee422f97f18336de9c4862dc698a0d67
Rich Moulton <rhmoult@gmail.com>
2025-08-17 23:51:06 +0000

    Initial commit

---
+++ Ch8/t_shirt.py
@@ -0,0 +1,6 @@
+def make_shirt(size="large", text="I love Python!"):
+    """Prints a message about the shirt size and color."""
+    print(f"A {size} shirt reading \'{text}\' has been created.")
+
+make_shirt()
+make_shirt(size="medium", text="Always look on the bright side of life.")
